12cbcbe1b686ba7edbf86e16fd4b4015441acad7
Shawn Kebel <fxrender@outlook.com>
2022-04-09 16:24:27 -0500

    :hammer: Add `store`

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import App from "./App";
+import "antd/dist/antd.css";
+import store from "./app/store";
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>,
-    // eslint-disable-next-line no-undef
+  // eslint-disable-next-line no-undef
   document.getElementById("root")
 );
