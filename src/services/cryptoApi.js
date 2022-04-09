c9c70a717645986cb5a561f73f5997a3d3cd9dd3
Shawn Kebel <fxrender@outlook.com>
2022-04-09 16:24:58 -0500

    :sparkles: Add `cryptoApi`

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+  "X-RapidAPI-Key": "6e640d2192msh78719a39ac0a8e8p16d9b0jsne89697fe5b9f",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com/";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
